1e9eb25453f3b503ef572b3f68d38e3af0c68f61
Sal <salem.ezz@gmail.com>
2018-09-11 15:45:38 -0400

    result page

--- src/views/search/search.js
+++ src/views/search/search.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import {
     Title, Media,
-    MediaContent,Columns,
+    MediaContent, Columns,
     Column, Card, CardContent, Container
 } from 'bloomer';
 import { Link } from 'react-router-dom'
 
-import { Image } from 'cloudinary-react';
+import { Image, CloudinaryContext } from 'cloudinary-react';
 import withRouter from '../../../node_modules/react-router-dom/withRouter';
 import $ from 'jquery'
 
@@ -79,32 +79,40 @@ class Search extends Component {
             <Container>
                 <Title style={{ textAlign: 'center', fontSize: 'calc(20px + 2vw)' }}>Results</Title>
                 <Columns>
-                    <Column isSize={8} isOffset={2}>
+                    <Column isSize={6} isOffset={3}>
                         <Card>
                             <CardContent>
-                                {this.state.searchResults.map((searchResult) => {
-                                    console.log(searchResult)
+                            {this.state.searchResults.map((searchResult) => {
+                                    console.log('image ' + searchResult.productName)
                                     return (
                                         <Media>
                                             <MediaContent>
-                                                <Title style={{ textAlign: 'center', fontSize: 'calc(8px + 2vw)' }}>{`${searchResult.productName}`}</Title>
-                                                <Image style={{ width: "auto" }} cloudName="dozenuld4" secure="true" publicId={searchResult.image} >
-                                                    {/* <Transformation width="300" height="100" crop="scale"/> */}
-                                                </Image>
-                                                <ul>
-                                                    <li><b>Quantity:</b> {searchResult.stock} </li>
-                                                    <li><b>Type:</b> {searchResult.type} </li>
-                                                    <li><b>User: </b>
-                                                        <Link to={{
-                                                            pathname: '/users/' + searchResult.creatorId,
-                                                            state: {
-                                                                profileUserID: searchResult.creatorId
-                                                            }
-                                                        }}>{searchResult.postedBy}</Link></li>
-                                                    <li><b>Details:</b> {searchResult.details} </li>
-                                                </ul>
+                                                <CloudinaryContext>
+                                                    <Title style={{ textAlign: 'center', fontSize: 'calc(8px + 2vw)' }}>{`${searchResult.productName}`}</Title>            
+                                                    <Image onClick={this.toggleModal} style={{ width: "auto", maxHeight: "auto" }} cloudName="dozenuld4" secure="true" publicId={searchResult.image}>
+                                                    </Image>
+                                                    {/* <Modal isActive={this.state.is_active}>
+                                                        <ModalBackground />
+                                                        <ModalContent>
+                                                            <Image style={{ width: "auto", maxHeight: "auto" }} cloudName="dozenuld4" secure="true" publicId={product.image}>
+                                                            </Image>
+                                                        </ModalContent>
+                                                        <ModalClose onClick={this.toggleModal} />
+                                                    </Modal> */}
+                                                    <ul>
+                                                        <li><b>Quantity:</b> {searchResult.stock} </li>
+                                                        <li><b>Type:</b> {searchResult.type} </li>
+                                                        <li><b>User: </b>
+                                                            <Link to={{
+                                                                pathname: '/users/' + searchResult.creatorId,
+                                                                state: {
+                                                                    profileUserID: searchResult.creatorId
+                                                                }
+                                                            }}>{searchResult.postedBy}</Link></li>
+                                                        <li><b>Details:</b> {searchResult.details} </li>                                     
+                                                    </ul>
+                                                </CloudinaryContext>
                                             </MediaContent>
-
                                         </Media>
                                     )
                                 })}
